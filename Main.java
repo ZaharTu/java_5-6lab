81bbc95d9b12d8dfbc3b950f477d7d365ed019c0
Туманов Захар Андреевич ИП-514 <zahar.tumanow@yandex.ru>
2025-10-22 21:45:49 +0700

    Revert "Add method getCarByBrendAndYearOperational"
    This reverts commit 630c2249031e63c26b568c9137986811a408c431.
    
    :q

--- Main.java
+++ Main.java
@@ -5,53 +5,27 @@ public class Main{
         Car[] cars = {
             new Car(1, "Toyota", "Camry", 2020, "White", 15000, "A123BC77"),
             new Car(2, "Toyota", "Madina", 2020, "Black", 45000, "B456DE77"),
-            new Car(3, "Toyota", "Corolla", 2011, "Red", 12000, "C789FG77"),
+            new Car(3, "Toyota", "Corolla", 2019, "Red", 12000, "C789FG77"),
             new Car(4, "Mercedes", "C-Class", 2021, "Silver", 38000, "D012HI77"),
             new Car(5, "Honda", "Civic", 2020, "Blue", 14000, "E345JK77")
         };
         System.out.println(Arrays.toString(getCarByBrend(cars, "Toyota")));
         System.out.println(Arrays.toString(getCarByBrend(cars, "123")));
-        System.out.println(Arrays.toString((getCarByBrendAndYearOperational(cars,"Toyota",2))));
     }
-    public static Car[] getCarByBrend(Car[] cars, String brend) {
-        int count = 0;
-        for (Car car : cars) {
-            if (car != null && car.getBrend().equals(brend)) {
+    public static Car[] getCarByBrend(Car[] cars, String brend){
+        int count=0;
+        for(Car car:cars){
+            if (car.getBrend().equals(brend)){
                 count++;
             }
         }
         Car[] ret_cars = new Car[count];
-        int index = 0;
-        for (Car car : cars) {
-            if (car != null && car.getBrend().equals(brend)) {
-                ret_cars[index++] = car;
+        int index=0;
+        for(Car car:cars){
+            if (car.getBrend().equals(brend)){
+                ret_cars[index++]=car;
             }
         }
         return ret_cars;
-    }
-    public static Car[] getCarByBrendAndYearOperational(Car[] cars, String brend, int years) {
-        int currentYear = 2025;
-        int count = 0;
-
-        for (Car car : cars) {
-            if (car != null && car.getBrend().equals(brend)) {
-                int age = currentYear - car.getYear();
-                if (age > years) {
-                    count++;
-                }
-            }
-        }
-        Car[] result = new Car[count];
-        int index = 0;
-        for (Car car : cars) {
-            if (car != null && car.getBrend().equals(brend)) {
-                int age = currentYear - car.getYear();
-                if (age > years) {
-                    result[index++] = car;
-                }
-            }
-        }
-
-        return result;
     }
 }
